1799a1f1eb767d53f7cb3a357fe509c65a74a17e
vedant dubey <dubeyvedant337@gmail.com>
2025-06-21 23:01:47 +0530

    commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,59 @@
+import User from "../models/User.model.js";
+import jwt from "jsonwebtoken";
+import mongoose  from "mongoose";
+
 export async  function signup(req,res){
-    const {email,password,fullName}=req.body
+    const {email,password,fullName}=req.body;
+    try{
+        if(!email||!password||!fullName){
+            return res.status(400).json({message:"all field are required"});
+
+        }
+    if(password.length<6){
+        return res.status(400).json({message:"password must be atleast 6 character"});
+
+    }
+    const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        return res.status(400).json({message:"invalid email format"});
+
+    }
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message:"email already exists"})
+    }
+
+    const idx=Math.floor(Math.random()*100)+1;
+    const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser=await User.create({
+        email,
+        fullName,
+        password,
+        profilePic:randomAvatar,
+    });
+
+    const token=jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+          expiresIn:"7d"
+    });
+    res.cookie("jwt",token,{
+        maxAge:7*24*60*60*1000,
+        httpOnly:true,
+        sameSite:"strict",
+        secure:process.env.NODE_ENV==="production",
+    })
+
+    res.status(201).json({success:true,user:newUser})
+
+    
+
+
+    }
+    catch(error){
+        console.log("error in signup controller ",error);
+        res.status(500).json({message:"internal server error"}); 
+
+    }
 }
 
 export async function login(req,res){
@@ -8,4 +62,4 @@ export async function login(req,res){
 }
  export function logout(req,res){
     res.send("logout route ");
- }
+ } 
